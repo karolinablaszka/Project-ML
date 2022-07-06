d183f43152f0fddcafd9df3e5b258873a3f964cc
Carol <kblaszka@edu.cdv.pl>
2022-07-06 22:54:33 +0200

    commit

--- dummy.py
+++ dummy.py
@@ -1,56 +1,54 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
 from sklearn.dummy import DummyClassifier
-from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
 import joblib 
 import warnings
 warnings.filterwarnings("ignore")
 
+def main():
 
+    X = joblib.load('train_data_2.pkl')
+    y = pd.read_csv(r'src\data\data_ML\train_labels.csv', header=None)
 
-X = joblib.load('train_data_2.pkl')
-y = pd.read_csv(r'src\data\data_ML\train_labels.csv', header=None)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-
-dummy_clf = DummyClassifier(strategy="most_frequent", random_state=42)
-dummy = dummy_clf.fit(X_train, y_train)
-y_pred = dummy_clf.predict(X_test)
-
-
-def classification(clf, X_train, y_train, X_test, y_test):
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
+    dummy_clf = DummyClassifier(strategy="most_frequent", random_state=42)
+    
+    def classification(clf, X_train, y_train, X_test, y_test):
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+            
+        #Classification report
+        print("CLASSIFICATION REPORT")
+        print("------------------------------------------")
+        print(classification_report(y_test, y_pred))
         
-    #Classification report
-    print("CLASSIFICATION REPORT")
-    print("------------------------------------------")
-    print(classification_report(y_test, y_pred))
-    
-    
-    #Plotting the normalized confusion matrix
-    cm = confusion_matrix(y_test, y_pred)
-    matrix = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = clf.classes_)
-    matrix.plot()
-    plt.show()
-    
+        
+        #Plotting the normalized confusion matrix
+        cm = confusion_matrix(y_test, y_pred)
+        matrix = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = clf.classes_)
+        matrix.plot()
+        plt.show()
+        
 
-classification(dummy_clf, X_train, y_train, X_test, y_test)
+    classification(dummy_clf, X_train, y_train, X_test, y_test)
 
-y_train.value_counts(normalize=True)
-# We clearly have a class imbalance problem
-# To address this we can SMOTE the training data and see 
-# if training a model with this method would improve our results.
+    y_train.value_counts(normalize=True)
+    # We clearly have a class imbalance problem
+    # To address this we can SMOTE the training data and see 
+    # if training a model with this method would improve our results.
 
-from imblearn.over_sampling import SMOTE
-sm = SMOTE()
-X_train_sm, y_train_sm = sm.fit_resample(X_train, y_train)
-y_train_sm.value_counts(normalize=True)
+    from imblearn.over_sampling import SMOTE
+    sm = SMOTE()
+    X_train_sm, y_train_sm = sm.fit_resample(X_train, y_train)
+    y_train_sm.value_counts(normalize=True)
 
-clf_dummy_sm = DummyClassifier()
-clf_dummy_sm.fit(X_train, y_train)
+    clf_dummy_sm = DummyClassifier()
+    clf_dummy_sm.fit(X_train, y_train)
 
-classification(clf_dummy_sm, X_train_sm, y_train_sm, X_test, y_test)
+    classification(clf_dummy_sm, X_train_sm, y_train_sm, X_test, y_test)
+
+if __name__ == '__main__':
+    main()
--- models.py
+++ models.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
 import joblib 
 import warnings
 from imblearn.over_sampling import SMOTE
@@ -14,92 +11,68 @@ from sklearn import svm
 
 warnings.filterwarnings("ignore")
 
-X = joblib.load('train_data_2.pkl')
-y = pd.read_csv(r'src\data\data_ML\train_labels.csv', header=None)
-y = y.values.ravel()
+def main():
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+    X = joblib.load('train_data_2.pkl')
+    y = pd.read_csv(r'src\data\data_ML\train_labels.csv', header=None)
+    y = y.values.ravel()
 
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train, y_train)
-X_test = scaler.transform(X_test)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
-smote = SMOTE()
-X_train, y_train = smote.fit_resample(X_train, y_train)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train, y_train)
+    X_test = scaler.transform(X_test)
 
-def classification(clf, X_train, y_train, X_test, y_test):
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-        
-    #Classification report
-    print("CLASSIFICATION REPORT")
-    print("------------------------------------------")
-    print(classification_report(y_test, y_pred))
-    
-    
-    #Plotting the normalized confusion matrix
-    cm = confusion_matrix(y_test, y_pred)
-    matrix = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = clf.classes_)
-    matrix.plot()
-    plt.show()
+    smote = SMOTE()
+    X_train, y_train = smote.fit_resample(X_train, y_train)
 
 
-#rfc_clf = RandomForestClassifier(random_state = 42, class_weight='balanced')
-#rfc_clf.fit(X_train, y_train)
-
-#classification(rfc_clf, X_train, y_train, X_test, y_test)
-
-#lo_clf = LogisticRegression(random_state=42, class_weight='balanced', solver='liblinear', multi_class='auto', max_iter=1000)
-#lo_clf.fit(X_train, y_train)
-
-#classification(lo_clf, X_train, y_train, X_test, y_test)
-
-model_params = {
-    'svm': {
-        'model': svm.SVC(gamma='auto'),
-        'params' : {
-            'C': [1,10,20],
-            'kernel': ['rbf','linear']
-        }  
-    },
-    'random_forest': {
-        'model': RandomForestClassifier(class_weight='balanced'),
-        'params' : {
-            'n_estimators': [1,5,10],
-            'classifier__max_features': [1, 2, 3],
-            "criterion": ["gini", "entropy"]
-        }
-    },
-    'logistic_regression' : {
-        'model': LogisticRegression(solver='liblinear', multi_class='auto', class_weight='balanced'),
-        'params': {
-            'C': [1,5,10]
-        }
-    },
-    'kn_classifier' : {
-        'model': KNeighborsClassifier(),
-        'params': {
-            'n_neighbors': [1,3,5]
+    model_params = {
+        'svm': {
+            'model': svm.SVC(gamma='auto'),
+            'params' : {
+                'C': [1,10,20],
+                'kernel': ['rbf','linear']
+            }  
+        },
+        'random_forest': {
+            'model': RandomForestClassifier(),
+            'params' : {
+                'n_estimators': [1,5,10]
+                        }
+        },
+        'logistic_regression' : {
+            'model': LogisticRegression(solver='liblinear', multi_class='auto'),
+            'params': {
+                'C': [1,5,10]
+            }
+        },
+        'kn_classifier' : {
+            'model': KNeighborsClassifier(),
+            'params': {
+                'n_neighbors': [1,3,5]
+            }
         }
     }
-}
 
 
 
 
-scores = []
+    scores = []
 
-for model_name, mp in model_params.items():
-    clf = GridSearchCV(mp['model'], mp['params'], cv=5, return_train_score=False, n_jobs=-1)
-    clf.fit(X, y.values.ravel())
-    scores.append({
-        'model': model_name,
-        'best_score': clf.best_score_,
-        'best_params': clf.best_params_
-    })
+    for model_name, mp in model_params.items():
+        clf = GridSearchCV(mp['model'], mp['params'], cv=5, return_train_score=False, n_jobs=-1)
+        clf.fit(X, y)
+        scores.append({
+            'model': model_name,
+            'best_score': clf.best_score_,
+            'best_params': clf.best_params_
+        })
 
 
-df = pd.DataFrame(scores,columns=['model','best_score','best_params'])
-print(df)
+    df = pd.DataFrame(scores,columns=['model','best_score','best_params'])
+    print(df)
 
+if __name__ == '__main__':
+    main()
 
