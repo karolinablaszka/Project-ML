2cb354501886ea1a320413e1f879966c0b729339
Carol <kblaszka@edu.cdv.pl>
2022-07-07 08:20:14 +0200

    commit

--- dummy.py
+++ dummy.py
@@ -12,7 +12,7 @@ def main():
     X = joblib.load('train_data_2.pkl')
     y = pd.read_csv(r'src\data\data_ML\train_labels.csv', header=None)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42, stratify=y)
 
     dummy_clf = DummyClassifier(strategy="most_frequent", random_state=42)
     
